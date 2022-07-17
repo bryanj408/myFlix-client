4adcd1799941cf846342acdac6eef5696d094577
bryanj408 <bryan.arndt52@gmail.com>
2022-07-16 20:15:04 -0700

    changed '' to " double quotes

--- src/components/login-view.jsx
+++ src/components/login-view.jsx
@@ -24,19 +24,21 @@ import axios from 'axios';
     });
   };
 
-return (
+  return (
     <Form>
-        <Form.Group controlId='formUsername'>
+      <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
         <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-        </Form.Group>
+      </Form.Group>
 
-        <Form.Group controlId="formPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+        </Button>
     </Form>
-)
-  }
+  )
+}
 
