74a4e0a74a61b31b67306a8ed04363d6d9f4de89
bryanj408 <bryan.arndt52@gmail.com>
2022-09-13 14:52:37 -0700

    fixed

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-<<<<<<< HEAD
 import { Form, Button, Card, Container, Col, Row, CardGroup } from 'react-bootstrap';
 import './registration-view.scss'
-=======
-import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
->>>>>>> gh-pages
+
 
 export function RegistrationView() {
 
@@ -48,7 +45,6 @@ export function RegistrationView() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-<<<<<<< HEAD
         const isReq = validate();
         if (isReq) {
             axios.post('https://myflixnetflix.herokuapp.com/users', {
@@ -105,107 +101,14 @@ export function RegistrationView() {
             </Row>
         </Container>
     )
-=======
-        axios.post('https://myflixnetflix.herokuapp.com/users', {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthday: birthday
-    })
-       .then(response => {
-        const data = response.data;
-        console.log(data);
-        window.open('/', '_self');
-       })
-       .catch(e => {
-        console.log('error registering the user');
-        alert('Something wasn\'t entered correctly');
-       });
-    };
-    
-
-   return (
-
-    <Container>
-        <Row>
-            <Col>
-                <CardGroup>
-                         <Card>
-                            <Card.Body>
-                            <Card.Title>Registration Page</Card.Title>
-                         <Form>
-                <Form.Group>
-                    <Form.Label>Username:</Form.Label>
-                        <Form.Control 
-                        type="text" 
-                        value={username} 
-                        onChange={e => setUsername(e.target.value)} 
-                        required 
-                        placeholder="Enter a username"
-                        />
-                </Form.Group>
-
-                <Form.Group>
-                <Form.Label>Password:</Form.Label>
-                        <Form.Control 
-                        type="password" 
-                        value={password} 
-                        onChange={e => setPassword(e.target.value)} 
-                        required 
-                        placeholder="Your password must be 8 or more characters"
-                        minLength="8"
-                        />
-                </Form.Group>
-
-                <Form.Group>
-                <Form.Label>Email:</Form.Label>
-                        <Form.Control 
-                        type="email" 
-                        value={email} 
-                        onChange={e => setEmail(e.target.value)} 
-                        required 
-                        placeholder="Enter your email address"
-                        />
-                </Form.Group>
-
-                <Form.Group>
-                <Form.Label>Birthday:</Form.Label>
-                        <Form.Control 
-                        type="birthdate" 
-                        value={birthday} 
-                        onChange={e => setBirthday(e.target.value)} 
-                        required 
-                        placeholder="Enter your birthday"
-                        />
-                </Form.Group>
-
-                <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-            </Form>
-                            </Card.Body>
-                           
-                         </Card>
-                </CardGroup>
-            </Col>
-        </Row>
-    </Container>
-   );
->>>>>>> gh-pages
 }
 
 RegistrationView.propTypes = {
     register: PropTypes.shape({
-<<<<<<< HEAD
         Username: PropTypes.string.isRequired,
         Password: PropTypes.string.isRequired,
         Email: PropTypes.string.isRequired,
         Birthday: PropTypes.number.isRequired
     })
 };
-=======
-      Username: PropTypes.string.isRequired,
-      Password: PropTypes.string.isRequired,
-      Email: PropTypes.string.isRequired,
-      Birthday: PropTypes.string
-    }),
-  };
->>>>>>> gh-pages
+
